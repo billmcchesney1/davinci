9b3e8cf885cb49370e90caf78f04b7a45eec0a0b
Circle CI <circleci@hp.com>
2020-12-03 13:28:47 +0000

    Update Docs

--- assets/js/app.09afc361.js
+++ assets/js/app.09afc361.js
@@ -388,15 +388,15 @@ __webpack_require__.r(__webpack_exports__);
  */
 /* harmony default export */ __webpack_exports__["default"] = ({
   "v-76bd1ed8": () => __webpack_require__.e(/*! import() */ 5).then(__webpack_require__.bind(null, /*! ./docs/README.md */ "./docs/README.md")),
-  "v-7831c59a": () => __webpack_require__.e(/*! import() */ 8).then(__webpack_require__.bind(null, /*! ./docs/guide/basics/controllers.md */ "./docs/guide/basics/controllers.md")),
+  "v-988e000c": () => __webpack_require__.e(/*! import() */ 6).then(__webpack_require__.bind(null, /*! ./docs/guide/README.md */ "./docs/guide/README.md")),
   "v-6ffafe83": () => __webpack_require__.e(/*! import() */ 7).then(__webpack_require__.bind(null, /*! ./docs/guide/basics/context.md */ "./docs/guide/basics/context.md")),
+  "v-7831c59a": () => __webpack_require__.e(/*! import() */ 8).then(__webpack_require__.bind(null, /*! ./docs/guide/basics/controllers.md */ "./docs/guide/basics/controllers.md")),
   "v-4ad616bb": () => __webpack_require__.e(/*! import() */ 9).then(__webpack_require__.bind(null, /*! ./docs/guide/basics/directory-structure.md */ "./docs/guide/basics/directory-structure.md")),
   "v-3ba6515b": () => __webpack_require__.e(/*! import() */ 10).then(__webpack_require__.bind(null, /*! ./docs/guide/basics/openapi-definitions.md */ "./docs/guide/basics/openapi-definitions.md")),
   "v-6d7ce972": () => __webpack_require__.e(/*! import() */ 4).then(__webpack_require__.bind(null, /*! ./docs/guide/basics/swagger-ui.md */ "./docs/guide/basics/swagger-ui.md")),
   "v-43676547": () => __webpack_require__.e(/*! import() */ 12).then(__webpack_require__.bind(null, /*! ./docs/guide/graphql/controllers.md */ "./docs/guide/graphql/controllers.md")),
   "v-ed006172": () => __webpack_require__.e(/*! import() */ 11).then(__webpack_require__.bind(null, /*! ./docs/guide/database/mongoose.md */ "./docs/guide/database/mongoose.md")),
-  "v-136c9927": () => __webpack_require__.e(/*! import() */ 13).then(__webpack_require__.bind(null, /*! ./docs/guide/graphql/getting-started.md */ "./docs/guide/graphql/getting-started.md")),
-  "v-988e000c": () => __webpack_require__.e(/*! import() */ 6).then(__webpack_require__.bind(null, /*! ./docs/guide/README.md */ "./docs/guide/README.md")) 
+  "v-136c9927": () => __webpack_require__.e(/*! import() */ 13).then(__webpack_require__.bind(null, /*! ./docs/guide/graphql/getting-started.md */ "./docs/guide/graphql/getting-started.md")) 
 });
 
 /***/ }),
@@ -464,12 +464,16 @@ const routes = [
     redirect: "/"
   },
   {
-    name: "v-7831c59a",
-    path: "/guide/basics/controllers.html",
+    name: "v-988e000c",
+    path: "/guide/",
     component: _home_circleci_project_node_modules_vuepress_core_lib_client_components_GlobalLayout_vue__WEBPACK_IMPORTED_MODULE_2__["default"],
     beforeEnter: (to, from, next) => {
-      Object(_app_util__WEBPACK_IMPORTED_MODULE_0__["ensureAsyncComponentsLoaded"])("Layout", "v-7831c59a").then(next)
+      Object(_app_util__WEBPACK_IMPORTED_MODULE_0__["ensureAsyncComponentsLoaded"])("Layout", "v-988e000c").then(next)
     },
+  },
+  {
+    path: "/guide/index.html",
+    redirect: "/guide/"
   },
   {
     name: "v-6ffafe83",
@@ -477,6 +481,14 @@ const routes = [
     component: _home_circleci_project_node_modules_vuepress_core_lib_client_components_GlobalLayout_vue__WEBPACK_IMPORTED_MODULE_2__["default"],
     beforeEnter: (to, from, next) => {
       Object(_app_util__WEBPACK_IMPORTED_MODULE_0__["ensureAsyncComponentsLoaded"])("Layout", "v-6ffafe83").then(next)
+    },
+  },
+  {
+    name: "v-7831c59a",
+    path: "/guide/basics/controllers.html",
+    component: _home_circleci_project_node_modules_vuepress_core_lib_client_components_GlobalLayout_vue__WEBPACK_IMPORTED_MODULE_2__["default"],
+    beforeEnter: (to, from, next) => {
+      Object(_app_util__WEBPACK_IMPORTED_MODULE_0__["ensureAsyncComponentsLoaded"])("Layout", "v-7831c59a").then(next)
     },
   },
   {
@@ -526,18 +538,6 @@ const routes = [
     beforeEnter: (to, from, next) => {
       Object(_app_util__WEBPACK_IMPORTED_MODULE_0__["ensureAsyncComponentsLoaded"])("Layout", "v-136c9927").then(next)
     },
-  },
-  {
-    name: "v-988e000c",
-    path: "/guide/",
-    component: _home_circleci_project_node_modules_vuepress_core_lib_client_components_GlobalLayout_vue__WEBPACK_IMPORTED_MODULE_2__["default"],
-    beforeEnter: (to, from, next) => {
-      Object(_app_util__WEBPACK_IMPORTED_MODULE_0__["ensureAsyncComponentsLoaded"])("Layout", "v-988e000c").then(next)
-    },
-  },
-  {
-    path: "/guide/index.html",
-    redirect: "/guide/"
   },
   {
     path: '*',
@@ -595,25 +595,40 @@ const siteData = {
       "lastUpdated": "11/15/2019, 1:02:47 PM"
     },
     {
-      "title": "Controllers",
+      "title": "Getting Started",
       "frontmatter": {},
-      "regularPath": "/guide/basics/controllers.html",
-      "relativePath": "guide/basics/controllers.md",
-      "key": "v-7831c59a",
-      "path": "/guide/basics/controllers.html",
+      "regularPath": "/guide/",
+      "relativePath": "guide/README.md",
+      "key": "v-988e000c",
+      "path": "/guide/",
       "headers": [
         {
           "level": 2,
-          "title": "Implements controller methods",
-          "slug": "implements-controller-methods"
+          "title": "Installation",
+          "slug": "installation"
         },
         {
           "level": 2,
-          "title": "Advanced cases (AKA: use express primitives)",
-          "slug": "advanced-cases-aka-use-express-primitives"
+          "title": "Start shaping the API",
+          "slug": "start-shaping-the-api"
+        },
+        {
+          "level": 3,
+          "title": "Create a controller",
+          "slug": "create-a-controller"
+        },
+        {
+          "level": 3,
+          "title": "Create the main file",
+          "slug": "create-the-main-file"
+        },
+        {
+          "level": 3,
+          "title": "Start the app",
+          "slug": "start-the-app"
         }
       ],
-      "lastUpdated": "11/13/2019, 1:36:59 PM"
+      "lastUpdated": "11/22/2019, 2:26:47 PM"
     },
     {
       "title": "Context",
@@ -635,6 +650,27 @@ const siteData = {
         }
       ],
       "lastUpdated": "11/13/2019, 3:44:46 PM"
+    },
+    {
+      "title": "Controllers",
+      "frontmatter": {},
+      "regularPath": "/guide/basics/controllers.html",
+      "relativePath": "guide/basics/controllers.md",
+      "key": "v-7831c59a",
+      "path": "/guide/basics/controllers.html",
+      "headers": [
+        {
+          "level": 2,
+          "title": "Implements controller methods",
+          "slug": "implements-controller-methods"
+        },
+        {
+          "level": 2,
+          "title": "Advanced cases (AKA: use express primitives)",
+          "slug": "advanced-cases-aka-use-express-primitives"
+        }
+      ],
+      "lastUpdated": "11/13/2019, 1:36:59 PM"
     },
     {
       "title": "Directory Structure",
@@ -764,42 +800,6 @@ const siteData = {
         }
       ],
       "lastUpdated": "5/8/2020, 5:11:59 PM"
-    },
-    {
-      "title": "Getting Started",
-      "frontmatter": {},
-      "regularPath": "/guide/",
-      "relativePath": "guide/README.md",
-      "key": "v-988e000c",
-      "path": "/guide/",
-      "headers": [
-        {
-          "level": 2,
-          "title": "Installation",
-          "slug": "installation"
-        },
-        {
-          "level": 2,
-          "title": "Start shaping the API",
-          "slug": "start-shaping-the-api"
-        },
-        {
-          "level": 3,
-          "title": "Create a controller",
-          "slug": "create-a-controller"
-        },
-        {
-          "level": 3,
-          "title": "Create the main file",
-          "slug": "create-the-main-file"
-        },
-        {
-          "level": 3,
-          "title": "Start the app",
-          "slug": "start-the-app"
-        }
-      ],
-      "lastUpdated": "11/22/2019, 2:26:47 PM"
     }
   ],
   "themeConfig": {
@@ -1148,7 +1148,7 @@ const { app, router } = Object(_app__WEBPACK_IMPORTED_MODULE_0__["createApp"])(f
 
 window.__VUEPRESS__ = {
   version: "1.3.0",
-  hash: "fb3b477"
+  hash: "f2af185"
 }
 
 router.onReady(() => {
@@ -14750,4 +14750,4 @@ module.exports = __webpack_require__(/*! /home/circleci/project/node_modules/@vu
 /***/ })
 
 /******/ });
-//# sourceMappingURL=app.8a35cec6.js.map
+//# sourceMappingURL=app.09afc361.js.map
--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "ad76c81a8af5dc46f6ba2ce87d0a40e1"
+    "revision": "bc952ea9d58f09a98e672e91767aa554"
   },
   {
     "url": "assets/css/styles.styles.58b7cb9e.css",
@@ -102,8 +102,8 @@ self.__precacheManifest = [
     "revision": "6229dbc5c07e16edf5b3ccd4aeceb1bb"
   },
   {
-    "url": "assets/js/app.8a35cec6.js",
-    "revision": "0ac57090bcc2c230e6212791432453f2"
+    "url": "assets/js/app.09afc361.js",
+    "revision": "babf932e776f8aba9cc7696c8a896ddf"
   },
   {
     "url": "assets/js/styles.58b7cb9e.js",
@@ -111,39 +111,39 @@ self.__precacheManifest = [
   },
   {
     "url": "guide/basics/context.html",
-    "revision": "86903782ad355726358343081dd0b660"
+    "revision": "bb491b136a457f2ea20ae39bb2672077"
   },
   {
     "url": "guide/basics/controllers.html",
-    "revision": "aae5bbcb6c05603d29e86edd7e2aa06b"
+    "revision": "218b7c70f7c0723486f559227dee3f0c"
   },
   {
     "url": "guide/basics/directory-structure.html",
-    "revision": "36416ca62be9ac04851957e6a63e5d70"
+    "revision": "9a8c15377e8943ee0390088540a9b570"
   },
   {
     "url": "guide/basics/openapi-definitions.html",
-    "revision": "70c8b30c9d92c8ecdc809c27031df8b4"
+    "revision": "784a879c3ab5ae226e9f40df4b2cfc3e"
   },
   {
     "url": "guide/basics/swagger-ui.html",
-    "revision": "b81e1db8b3399bc09cec18d041096135"
+    "revision": "50014b3e4de7891c75a4f4f6f2402807"
   },
   {
     "url": "guide/database/mongoose.html",
-    "revision": "7c7436294bbcebc0a6d5bffd98cbd50a"
+    "revision": "4a99313757387fd2023857b8b4aee1f9"
   },
   {
     "url": "guide/graphql/controllers.html",
-    "revision": "fc8fb55c21846843f09ed9afa8f2e3fa"
+    "revision": "0a2cf97fd631b04c6c245cab3c3b885c"
   },
   {
     "url": "guide/graphql/getting-started.html",
-    "revision": "7a589196a38375048b925a20de71771f"
+    "revision": "32fd3df5af5225731055624561c8b6d3"
   },
   {
     "url": "guide/index.html",
-    "revision": "c25519e43b58f8ccf8fdde8847817dfa"
+    "revision": "59a9eaa49cd5c8177c842586eaa42592"
   },
   {
     "url": "images/basics/swagger-ui/swagger-ui-screenshot.png",
@@ -155,7 +155,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "c7fdd046263c4f8bd066f0a60731ad26"
+    "revision": "5f98efa760f9dec6ee6eb95b544fac78"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
